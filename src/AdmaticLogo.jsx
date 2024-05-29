0dab88e72fb788cea7f244adedc35ff4b7cc6584
Marc Obel <marc.obel@outlook.com>
2024-05-29 11:23:34 +0200

    updated logo

--- src/AdmaticLogo.jsx
+++ src/AdmaticLogo.jsx
@@ -7,7 +7,7 @@ import { IconButton } from '@mui/material';
 function AdmaticLogo() {
   return (
     <IconButton component="a" href="https://admatic.dk" target="_blank">
-      <img src="https://admatic.dk/wp-content/uploads/logo-admatic.svg" alt="Admatic Logo" style={{ width: 200, height:200 }} />
+      <img src="https://admatic.dk/wp-content/uploads/logo-admatic.svg" alt="Admatic Logo" style={{ width: 200, height:140 }} />
     </IconButton>
   );
 }
